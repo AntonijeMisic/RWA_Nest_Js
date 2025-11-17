d10ccbe86cef405f32f10aec367435d32546f3bc
Antonije Misic <antonije.misic@accordia-group.com>
2025-11-17 14:03:28 +0100

    Notifications and FitPass-usage removed from project.

--- back-end/src/database/database.providers.ts
+++ back-end/src/database/database.providers.ts
@@ -1,15 +1,12 @@
 import { ConfigService } from '@nestjs/config';
 import { Announcement } from 'src/announcements/announcement.entity';
 import { RefreshToken } from 'src/auth/refreshToken/refreshToken.entity';
-import { FitPassUsage } from 'src/fitpass-usage/fitpassUsage.entity';
 import { LeaveRequest } from 'src/leave-requests/leaveRequest.entity';
 import { LeaveType } from 'src/lookups/entities/leaveType.entity';
 import { RequestStatus } from 'src/lookups/entities/requestStatus.entity';
 import { UserPosition } from 'src/lookups/entities/userPosition.entity';
 import { UserRole } from 'src/lookups/entities/userRole.entity';
 import { WorkType } from 'src/lookups/entities/workType.entity';
-import { Notification } from 'src/notifications/notification.entity';
-import { NotificationUser } from 'src/notifications/notificationUser.entity';
 import { User } from 'src/users/user.entity';
 import { WorkLog } from 'src/worklogs/worklog.entity';
 import { DataSource } from 'typeorm';
@@ -19,9 +16,6 @@ const entities = [
   UserRole,
   LeaveRequest,
   Announcement,
-  FitPassUsage,
-  Notification,
-  NotificationUser,
   RequestStatus,
   WorkLog,
   UserPosition,
--- back-end/src/fitpass-usage/fitpass-usage.controller.ts
+++
@@ -1,4 +0,0 @@
-import { Controller } from '@nestjs/common';
-
-@Controller('fitpass-usage')
-export class FitpassUsageController {}
